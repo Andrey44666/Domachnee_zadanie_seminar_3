50daa0edf01e22262730cd3280dada0dd1e8b7ec
Oleynik Andrey <andry44666@gmail.com>
2023-02-04 02:28:06 +0300

    Complited Zadacha_3_seminar_3

---
+++ Zadacha_3_seminar_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int N = 1;
+while (N <= number)
+{
+    System.Console.Write(N * Math.Pow(N, 3) + " ");
+    N+=1;
+}   
