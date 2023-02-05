34095cdd2369c7bb2294abb082c63dc5ddf8215f
Oleynik Andrey <andry44666@gmail.com>
2023-02-06 01:53:04 +0300

    Complited Zadacha_1_seminar_3

---
+++ Zadacha_1_seminar_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+System.Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int N = 0;
+int default_number = number;
+while (number > 0) 
+{
+   N *= 10;     
+   N += number % 10;
+   number /= 10;
+}
+Console.WriteLine(N);
+if (N == default_number)
+{
+   Console.Write("number palindrome");
+}
+else
+{
+   Console.Write("number not palindrome");   
+}
